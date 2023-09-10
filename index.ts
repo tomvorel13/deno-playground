fe751b842b5365989fca5f7d9847dd181ddf3f2f
Tom Vorel <tomvorel13@gmail.com>
2023-09-10 19:13:22 +0200

    Using a router

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-import { Application } from "oak";
+import { Application, Router } from "oak";
 
 const app = new Application();
+const router = new Router();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+router.get("/", (ctx) => {
+  ctx.response.status = 200;
+  ctx.response.body = "Hello world AGAIN!";
 });
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 await app.listen({ port: 3000 });
